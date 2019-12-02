bf6445703d8c5510d1dd6a0234b81cdfef225905
Gilles-Martin <gilles.pba@gmail.com>
2019-12-02 12:11:00 +0100

    Adding ArticleControler AND FactureController

--- routes/web.php
+++ routes/web.php
@@ -10,22 +10,10 @@
   | contains the "web" middleware group. Now create something great!
   |
  */
-Route::get('{n}', function($n) {
-    return response('Je suis la page ' . $n . ' !', 200);
-})->where('n', '[1-3]');
 
-Route::get('/', ['as' => 'home', function()
-{
-  return 'Je suis la page d\'accueil !';
-}]);
-Route::get('/', function () {
-    return view('vue1');
-});
 
-Route::get('/article/{n}', function ($n) {
-    return view('article',['numero' => $n]);
-})->where('n', '[0-9]+');
+Route::get('/',  ['uses' => 'WelcomeController@index', 'as' => 'home']);
 
-Route::get('/facture/{n}', function ($n) {
-    return view('facture',['numero' => $n]);
-})->where('n', '[0-9]+');
+Route::get('/article/{n}',  ['uses' => 'ArticleController@show', 'as' => 'article'])->where('n', '[0-9]+');
+
+Route::get('/facture/{n}',[ 'uses' => 'FactureController@show' ])->where('n', '[0-9]+');
