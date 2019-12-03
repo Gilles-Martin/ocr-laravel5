c6b50f476273ce47209cec1b314b01a383c64a34
Gilles-Martin <gilles.pba@gmail.com>
2019-12-03 16:02:05 +0100

    Clean and indent file UserRepository

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -10,8 +10,18 @@ class UserRepository extends ResourceRepository
     protected $user;
 
     public function __construct(User $user)
-	{
-		$this->model = $user;
-	}
+    {
+        $this->model = $user;
+    }
+
+    public function store(Array $inputs)
+    {
+        $user = new $this->user;
+        $user->password = bcrypt($inputs['password']);
+
+        $this->save($user, $inputs);
+
+        return $user;
+    }
 
 }
